f7d863ac4f8e41300b8fa3b008ef3bdbfd428fd1
GouravSapra <97378735+GouravSapra@users.noreply.github.com>
2022-01-09 12:47:18 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 CACHE = {
     0: [
-        "https://images.unsplash.com/photo-1531323386183-43890b5c766d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-        "First Product",
-        "Product Purchase Link"
+        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcS9FhqzZNBdP9q2t4bpyHi5W0j_U3XAWTQw4qsKQ5ZgEpVDkqHjPQjE3cpY5YTcMdLgIH_mv0gEPpJq944XiPm4VfL66cKd0Q7MlnlR9JY2KI8bVFLvY3DREA&usqp=CAE",
+        "Alexa Speaker",
+        "https://www.google.com/aclk?sa=l&ai=DChcSEwi2uOv4j6T1AhXqg0sFHWXnA4MYABABGgJzZg&sig=AOD64_1KMaIQVmhZug1anyXBN-YP8toPrA&ctype=5&q=&ved=0ahUKEwi74Of4j6T1AhVEjeYKHcQhB8AQww8Ingk&adurl="
     ],
     1: [
         "https://thumbs.dreamstime.com/z/beautiful-happy-woman-showing-love-sign-near-eyes-healthy-vision-portrait-holding-heart-shaped-hands-closeup-smiling-83939671.jpg",
